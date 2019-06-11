52c8829fe3ba41c5d7c652af0334d7171f575765
ohel <ohel@users.noreply.github.com>
2019-06-12 01:47:52 +0300

    Title override hack.

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@
 
 piodev="1A86:7523"
 
+echo Use web/wifi.cfg to setup WiFi.
+echo Use web/title.cfg to override index.html titles.
+
 [ ! -e src/ledsetup.h ] && touch src/ledsetup.h
 wifi_cfg_in="web/wifi.cfg"
 wifi_cfg_out="src/wificonfig.h"
@@ -20,8 +23,18 @@ echo ${wifi_pass:-"password"} | sed "s/\(.*\)/const char* _WIFI_PASSWORD = \"\0\
 echo ${wifi_ip:-"0.0.0.0"} | sed "s/\./, /g" | sed "s/\(.*\)/const IPAddress _WIFI_IP(\0);/" >> $wifi_cfg_out
 echo ${wifi_ip:-"0.0.0.0"} | sed "s/\./, /g" | sed "s/[0-9]\{1,\}$/1/" | sed "s/\(.*\)/const IPAddress _WIFI_GATEWAY(\0);/" >> $wifi_cfg_out
 
+if [ -e web/title.cfg ]
+then
+    tmpfile=$(mktemp)
+    cp web/index.html $tmpfile
+    title=$(cat web/title.cfg)
+    sed -i "s/__LEDSTRIP__/$title/g" web/index.html
+fi
+
 xxd -i web/index.html | sed 's/\([0-9a-f]\)$/\0, 0x00/' > src/index.h
 xxd -i web/favicon.ico > src/favicon.h
+
+[ -e web/title.cfg ] && [ -e $tmpfile ] && cp $tmpfile web/index.html && rm $tmpfile 2>/dev/null
 
 port=$(pio device list | grep -B 2 "VID:PID=$piodev" | head -n 1)
 [ $port ] && pio run -t upload --upload-port $port || pio run
